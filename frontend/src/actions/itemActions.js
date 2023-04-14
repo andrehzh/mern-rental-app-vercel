4334e19c900746d2a8a8b799a362eab3a3c85182
andrehzh <e0725806@u.nus.edu>
2023-04-14 13:08:57 +0800

    Update itemActions.js

--- frontend/src/actions/itemActions.js
+++ frontend/src/actions/itemActions.js
@@ -133,7 +133,11 @@ export const addItem = (formData) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.post("/api/items", formData, config);
+    const { data } = await axios.post(
+      "http://localhost:4000/api/items",
+      formData,
+      config
+    );
 
     dispatch({ type: ITEM_ADD_SUCCESS, payload: data });
     localStorage.setItem("itemInfo", JSON.stringify(data));
