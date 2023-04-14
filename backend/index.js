0eba631946f1ad7dd80ac41db21622ecd7e5c23a
andrehzh <e0725806@u.nus.edu>
2023-04-14 13:32:16 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,14 @@ app.use(express.json());
 app.use(cors(corsOrigin));
 
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   res.send("API is running...");
 });
 
