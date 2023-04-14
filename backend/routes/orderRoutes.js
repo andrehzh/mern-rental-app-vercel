6505f24b75250a2aafb8fa2f89042fbb272b140b
andrehzh <e0725806@u.nus.edu>
2023-04-14 12:26:46 +0800

    ItemController Deployment Test

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,24 +1,24 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
-//   updateOrderToDelivered,
+  //   updateOrderToDelivered,
   getMyItemOrders,
   getMyOrders,
   updateOrder,
-//   getOrders,
-} from '../controllers/orderController.js'
-import { protect, /*admin*/ } from '../middleware/authMiddleware.js'
+  //   getOrders,
+} from "../controllers/orderController.js";
+import { protect /*admin*/ } from "../middleware/authMiddleware.js";
 
-router.route('/').post(protect, addOrderItems)
+router.route("/").post(protect, addOrderItems);
 // router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
-router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/myorders').get(protect, getMyOrders)
-router.route('/:userId').get(protect, getMyItemOrders)
-router.route('/myorders/:id').get(protect, getOrderById)
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:userId").get(protect, getMyItemOrders);
+router.route("/myorders/:id").get(protect, getOrderById);
 router.route("/:id").put(updateOrder);
 // router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
-export default router
+export default router;
